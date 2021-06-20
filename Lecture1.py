a0128e2045a4936a48baa7a2e610da44a81606fb
eminkartci <emin.kartci@ozu.edu.tr>
2021-06-20 22:44:42 +0300

    Java comparison

--- Lecture1.py
+++ Lecture1.py
@@ -1,1 +1,18 @@
-print("Hello World")
+# print("Hello World")
+
+# x = "12"
+# y = 150
+# print(x,y+y,sep="\n")
+
+# x = 50
+# print(x,x**2)
+
+# i = 12
+# for i in [1,3,5,6]:
+#     print(i)
+
+# print(i)
+
+num1 = int(input("num1: ")) 
+num2 = int(input("num2: "))
+print(num1 + num2)
